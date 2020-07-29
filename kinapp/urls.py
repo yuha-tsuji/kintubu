2e124d4e3a4257a26f180f9d4bf5aeefd5dd72d0
yuha-tsuji <yuga7517@gmail.com>
2020-07-29 17:16:45 +0900

    新規作成機能の作成

--- kinapp/urls.py
+++ kinapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HomeView
+from .views import HomeView, CreateKintore
 
 urlpatterns = [
-    path('home', HomeView.as_view(), name='home')
+    path('home', HomeView.as_view(), name='home'),
+    path('create', CreateKintore.as_view(), name='create')
 ]
--- kinapp/views.py
+++ kinapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
 
 from .models import KintoreModel
 
@@ -7,4 +8,18 @@ from .models import KintoreModel
 
 class HomeView(ListView):
 	template_name='home.html'
+	# model = KintoreModel
+
+	def get(self, request):
+		qs = KintoreModel.objects.filter(user=request.user)
+		return render(request, self.template_name, {'objectsList': qs})
+
+class CreateKintore(CreateView):
+	template_name = 'create.html'
 	model = KintoreModel
+	fields = ('bui', 'menu', 'kaisu', 'setsu')
+	success_url = reverse_lazy('home')
+
+	def form_valid(self, form):
+		form.instance.user = self.request.user
+		return super().form_valid(form)
